09c1a06540eb1ad296d040735932ba5263b42259
TuanKiet960 <truongkietminecraft@gmail.com>
2026-02-11 08:29:24 +0700

    Update Code User Management
    Update Code User Management

---
+++ EyewearStore_SWP391/Pages/Admin/Users/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EyewearStore_SWP391.Pages.Admin.Users
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ EyewearStore_SWP391/Pages/Admin/Users/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EyewearStore_SWP391.Pages.Admin.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
